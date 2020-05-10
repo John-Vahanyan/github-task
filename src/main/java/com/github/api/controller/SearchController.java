d8619eab964453e6390e5b810967d68a4dbfc705
Jon Vahanyan <jon.vahanyan@iunetworks.am>
2020-05-11 01:54:26 +0400

    minor bug

--- src/main/java/com/github/api/controller/SearchController.java
+++ src/main/java/com/github/api/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.github.api.controller;
 import com.github.api.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
     private final SearchService searchService;
 
-    @RequestMapping("/repositories")
+    @GetMapping("/repositories")
     public ResponseEntity<?> searchRepositories(@RequestParam("text") String text,
                                                 @RequestParam("page") Integer page,
                                                 @RequestParam("perPage") Integer perPage) {
         return ResponseEntity.ok(searchService.searchRepositories(text, page, perPage));
     }
 
-    @RequestMapping("/commits")
+    @GetMapping("/commits")
     public ResponseEntity<?> searchCommits(@RequestParam("owner") String owner,
                                            @RequestParam("repo") String repo) {
         return ResponseEntity.ok(searchService.searchRepositoryCommits(owner, repo));
